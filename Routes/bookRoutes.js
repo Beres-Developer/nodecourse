a240efee1cdb49dea05e179ed9f9c28d52861bcc
Beres McFarlane <beresmcfarlane@beress-air.home>
2018-02-25 10:22:10 +0000

    Add DELETE /books/:id route.

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -76,7 +76,16 @@ bookRouter.route('/:bookId')
                 res.json(req.book);
             }
         });
-    });
+    })
+    .delete(function(req, res){
+        req.book.remove(function(err){
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(204).send('Removed');
+            }
+        })
+    })
 
     return bookRouter;
 };
