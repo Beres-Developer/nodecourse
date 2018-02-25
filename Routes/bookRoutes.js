f4bc5cbcedca0ed03597b87cc03edf6771e5a4c4
Beres McFarlane <beresmcfarlane@beress-air.home>
2018-02-25 10:16:12 +0000

    Add PUT /books/:id and PATCH /books/:id routes.

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -24,28 +24,57 @@ bookRouter.route('/')
         });
     });
 
+bookRouter.use('/:bookId', function(req, res, next) {
+    Book.findById(req.params.bookId, function(err, book){
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            if(book){
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
+        });
+});
+
 bookRouter.route('/:bookId')
     .get(function(req, res){
-
-        Book.findById(req.params.bookId, function(err, book){
-            if (err) {
-                return res.status(500).send(err);
-            }
-
-            res.json(book);
-        });
+        res.json(req.book);
     })
     .put(function(req, res){
-        Book.findById(req.params.bookId, function(err, book){
+        
+        req.book.title = req.body.title;
+        req.book.author = req.body.author;
+        req.book.genre = req.body.genre;
+        req.book.read = req.body.read;
+        req.book.save(function(err){
             if (err) {
-                return res.status(500).send(err);
+                res.status(500).send(err);
+            } else {
+                res.json(req.book);
             }
-            book.title = req.body.title;
-            book.author = req.body.author;
-            book.genre = req.body.genre;
-            book.read = req.body.read;
-            book.save();
-            res.json(book);
+        });
+        res.json(req.book);
+    })
+    .patch(function(req, res){
+        // if(req.body.title) {
+        //     req.book.title = req.body.title;
+        // }
+        if (req.body._id) {
+            delete req.body._id;
+        }
+        for(let p in req.body) {
+            req.book[p] = req.body[p];
+        }
+
+        req.book.save(function(err){
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(req.book);
+            }
         });
     });
 
